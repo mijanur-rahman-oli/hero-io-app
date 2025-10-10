fba7d793f4d405343994b56ea5f704a6d8eccde4
mijanur-rahman-oli <sheikhmijanurrahmanoli@gmail.com>
2025-10-10 11:56:43 +0600

    custom hook

---
+++ src/Hooks/useAppList.jsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const useAppList = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("/appList.json")
+      .then((res) => setProducts(res.data))
+      .catch((err) =>
+        setError({ message: err.message || "Failed to load app data" })
+      )
+      .finally(() => setLoading(false));
+  }, []);
+
+  return { products, loading, error };
+};
+
+export default useAppList;
+
+
